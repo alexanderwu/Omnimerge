89d16e6f7f7c7bbbd1c8491507ff9ea67f67b034
Alexander Wu <alexander.wu7@gmail.com>
2020-09-13 23:02:37 -0700

    Input filenames with input.txt (currently hardcoded to 8 files)

--- merge_files.py
+++ merge_files.py
@@ -12,16 +12,20 @@ import pandas as pd
 import pandas.io.formats.excel
 pandas.io.formats.excel.ExcelFormatter.header_style = None
 
+INPUT_FILE = 'input.txt'
 OUTPUT_PATH = 'merged.xlsx'
 
-dir1_file1_df = pd.read_csv('dir1/file1.csv')
-dir1_file2_df = pd.read_csv('dir1/file2.csv')
-dir2_file1_df = pd.read_csv('dir2/file1.csv')
-dir2_file2_df = pd.read_csv('dir2/file2.csv')
-dir3_file1_df = pd.read_csv('dir3/file1.csv')
-dir3_file2_df = pd.read_csv('dir3/file2.csv')
-dir4_file1_df = pd.read_csv('dir4/file1.csv')
-dir4_file2_df = pd.read_csv('dir4/file2.csv')
+input_df = pd.read_csv('input.txt', header=None)
+input_list = input_df[0].values
+
+dir1_file1_df = pd.read_csv(input_list[0])
+dir1_file2_df = pd.read_csv(input_list[1])
+dir2_file1_df = pd.read_csv(input_list[2])
+dir2_file2_df = pd.read_csv(input_list[3])
+dir3_file1_df = pd.read_csv(input_list[4])
+dir3_file2_df = pd.read_csv(input_list[5])
+dir4_file1_df = pd.read_csv(input_list[6])
+dir4_file2_df = pd.read_csv(input_list[7])
 
 dir1_file1_df[''] = None
 sheet1_df = pd.concat([dir1_file1_df, dir1_file2_df], axis=1)
@@ -33,7 +37,7 @@ dir4_file1_df[''] = None
 sheet4_df = pd.concat([dir4_file1_df, dir4_file2_df], axis=1)
 
 with pd.ExcelWriter(OUTPUT_PATH) as writer:
-    sheet1_df.to_excel(writer, sheet_name='Sheet1', index=False)
-    sheet2_df.to_excel(writer, sheet_name='Sheet2', index=False)
-    sheet3_df.to_excel(writer, sheet_name='Sheet3', index=False)
-    sheet4_df.to_excel(writer, sheet_name='Sheet4', index=False)
+    sheet1_df.to_excel(writer, sheet_name='Sheet1', index=False)
+    sheet2_df.to_excel(writer, sheet_name='Sheet2', index=False)
+    sheet3_df.to_excel(writer, sheet_name='Sheet3', index=False)
+    sheet4_df.to_excel(writer, sheet_name='Sheet4', index=False)
